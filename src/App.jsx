db80ab5db7b983fb127a3fe72b2c13ee0e64c83a
wasiqanwar <wasiq.anwar21@gmail.com>
2023-08-14 23:30:46 +0500

    update build for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { InventoryUpdate } from "./pages/InventoryUpdate";
+import { InventoryUpdate } from "./pages/InventoryUpdate.jsx";
 
 function App() {
   return (
