c7f7c3d2755efd1f41122abcf04ba115c27e3af9
wasiqanwar <wasiq.anwar21@gmail.com>
2023-08-14 20:33:49 +0500

    update build for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { InventoryUpdate } from "./pages/InventoryUpdate";
 
 function App() {
-
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<InventoryUpdate />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<InventoryUpdate />} />
+      </Routes>
+    </Router>
   );
 }
 
