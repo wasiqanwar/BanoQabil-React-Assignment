b0008fcfe690c567723a3b96f75f480db790af4c
wasiqanwar <wasiq.anwar21@gmail.com>
2023-08-14 18:30:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { InventoryUpdate } from "./pages/InventoryUpdate";
+
+function App() {
+
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<InventoryUpdate />} />
+          {/* <Route path="/edit" element={} /> */}
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
